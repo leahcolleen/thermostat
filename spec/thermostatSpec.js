c79e36b1328814e4f0aae1bacad9d5e0ec6b376e
Leah <leahpitter95@gmail.com>
2021-09-28 17:03:37 +0100

    minimum temperature added, tests pass

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,4 +21,11 @@ describe ('Thermostat', () => {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
+
+  it('has a minimum of 10 degrees', () => {
+    for (let i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
 });
